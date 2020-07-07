24f0b9ee5b68945432c20860bad8e2d50d4314ea
Yuri Thomas <yuri.tpinheiro@gmail.com>
2020-07-06 22:54:33 -0300

    gambi

---
+++ docs/precache-manifest.fbdea52bf643d49c7594fa48739d5fa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34040dd902dd447d4e19ce7c45215ef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5381c479edb32c293651",
+    "url": "/static/css/main.f4d03a0c.chunk.css"
+  },
+  {
+    "revision": "ec2c6eadfd4aa619f3a5",
+    "url": "/static/js/2.d8b36e27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d8b36e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5381c479edb32c293651",
+    "url": "/static/js/main.1b48f776.chunk.js"
+  },
+  {
+    "revision": "f82865d05e16b25d7dc3",
+    "url": "/static/js/runtime-main.b019f3b3.js"
+  },
+  {
+    "revision": "0defd1993f02912c0c80938a3b34bb9b",
+    "url": "/static/media/image.0defd199.png"
+  }
+]);
