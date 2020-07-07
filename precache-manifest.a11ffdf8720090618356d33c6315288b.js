5cab83dc56bbee64945f6c84abf7077f52b9ddcc
yuritpinheiro <yuritpinheiro@users.noreply.github.com>
2020-07-07 02:22:22 +0000

    deploy: d1c914ccbfb8a22526d954b29d62464fdb0ca494

---
+++ precache-manifest.a11ffdf8720090618356d33c6315288b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2373b0033bdd38d886d9ef561af94c",
+    "url": "/poke_store/index.html"
+  },
+  {
+    "revision": "e7252825404a0418bad3",
+    "url": "/poke_store/static/css/main.195998c0.chunk.css"
+  },
+  {
+    "revision": "79b41ae905d41cc99138",
+    "url": "/poke_store/static/js/2.362b4c5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/poke_store/static/js/2.362b4c5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7252825404a0418bad3",
+    "url": "/poke_store/static/js/main.120861fd.chunk.js"
+  },
+  {
+    "revision": "741c5a46d01660eadf88",
+    "url": "/poke_store/static/js/runtime-main.6a2e15a4.js"
+  },
+  {
+    "revision": "0defd1993f02912c0c80938a3b34bb9b",
+    "url": "/poke_store/static/media/image.0defd199.png"
+  }
+]);
